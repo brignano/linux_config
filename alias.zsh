4f3f829a6d85dafaa769929ad3dbfaee0ea92660
anthony <brignano@users.noreply.github.com>
2020-07-29 22:04:11 -0400

    Create alias.zsh

---
+++ alias.zsh
@@ -0,0 +1,8 @@
+# change directories
+alias proj="cd ~/projects"
+alias docs="cd ~/Documents"
+alias desk="cd ~/Desktop"
+
+alias eb="code ~/.oh-my-zsh/"
+alias ub="source ~/.zshrc"
+alias c="clear"
