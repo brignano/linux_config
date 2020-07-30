ba609783d773520142ff8e24eb6cced0cbadbf9c
anthony <brignano@users.noreply.github.com>
2020-07-29 23:27:11 -0400

    Update and rename test to aliases.zsh

---
+++ aliases/aliases.zsh
@@ -0,0 +1,12 @@
+# change directories
+alias proj="cd ~/projects"
+alias docs="cd ~/Documents"
+alias desk="cd ~/Desktop"
+
+# shortcuts
+alias c="clear"
+alias o="nautilus ."
+
+# edit/update oh-my-zsh
+alias editbash="code ~/.oh-my-zsh/"
+alias updatebash="source ~/.zshrc"
